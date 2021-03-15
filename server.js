13088a1e8a874efcd507ef3e805fa40158472b09
jaynedoe <jaedoyne@hotmail.com>
2021-03-16 09:09:49 +1100

    corrected file paths for app.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/app/controllers"))
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-})
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.  Press Ctrl + C to exit.`);
