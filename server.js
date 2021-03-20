0327836dfb195c993006c2b1aa43e55e1b5410d3
jaynedoe <jaedoyne@hotmail.com>
2021-03-20 15:16:44 +1100

    added path to public folder javascript

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/app/views/public"));
+app.use(express.static(__dirname + "/app/views/public/css"));
+app.use(express.static(__dirname + "/app/views/public/js"));
 app.use(express.static(__dirname + "/app/controllers"));
 
 app.get('/', function (req, res){
